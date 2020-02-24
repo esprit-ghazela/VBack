2421b4f35321b26ec19190891ab9e06c5b9378eb
youssrra <yousra.abid@esprit.tn>
2020-02-24 23:21:56 +0100

    admmin

--- src/UserBundle/Controller/AdminController.php
+++ src/UserBundle/Controller/AdminController.php
@@ -12,10 +12,10 @@ class AdminController extends Controller
     public function afficherAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
-        $partenaires=$em->getRepository('UserBundle:User')->RoleAdmin();
+        $admins=$em->getRepository('UserBundle:User')->RoleAdmin();
 
         return $this->render('@VBack/Template/gestion_administrateur.html.twig',array(
-            'partenaires'=>$partenaires
+            'admins'=>$admins
         ));
     }
 
